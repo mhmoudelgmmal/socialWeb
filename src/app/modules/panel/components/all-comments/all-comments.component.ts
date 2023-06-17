8eb0a70c9b422fd92ef7c51cdd04e8806bc2c768
mhmoudelgmmal <mhmoudashour6@gmail.com>
2023-06-17 14:39:02 +0300

    start the all comments component work

--- src/app/modules/panel/components/all-comments/all-comments.component.ts
+++ src/app/modules/panel/components/all-comments/all-comments.component.ts
@@ -1,10 +1,27 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
+import { PanelService } from '../../services/panel.service';
+import { Router } from '@angular/router';
 
 @Component({
   selector: 'app-all-comments',
   templateUrl: './all-comments.component.html',
   styleUrls: ['./all-comments.component.css']
 })
-export class AllCommentsComponent {
+export class AllCommentsComponent implements OnInit {
+  allComments:any
+  constructor(private panelServeice:PanelService,
+    private _router:Router,){}
 
+  ngOnInit(): void {
+
+  }
+  getComments(){
+    this.panelServeice.getAllComments().subscribe(
+      (res)=>{
+        this.allComments = res
+      },(error)=>{
+
+      }
+    )
+  }
 }
